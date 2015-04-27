42c1633675df9b0d2b14a3cf58edbfb79d2aec9a
valkjsaaa <jackieyang51@gmail.com>
2015-04-27 20:58:44 +0800

    Lab 4: Exercise 3

--- kern/pmap.c
+++ kern/pmap.c
@@ -285,6 +285,16 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	//
 	// LAB 4: Your code here:
+	int i;
+	for (i = 0; i < NCPU; ++i)
+	{
+		uintptr_t kstacktop_i = KSTACKTOP - i * (KSTKSIZE + KSTKGAP);
+		boot_map_region(kern_pgdir,
+		                kstacktop_i - KSTKSIZE,
+		                KSTKSIZE,
+		                PADDR(percpu_kstacks[i]),
+		                PTE_P|PTE_W);
+	}
 }
 
 // --------------------------------------------------------------
