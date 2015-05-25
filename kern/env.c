d4668a4ce338b0cae403689fa3851a7994c6343c
valkjsaaa <jackieyang51@gmail.com>
2015-05-25 18:40:11 +0800

    Lab 5: Exercise 1

--- kern/env.c
+++ kern/env.c
@@ -418,6 +418,10 @@ env_create(uint8_t *binary, enum EnvType type)
 
 	// If this is the file server (type == ENV_TYPE_FS) give it I/O privileges.
 	// LAB 5: Your code here.
+	if (type == ENV_TYPE_FS)
+	{
+		e->env_tf.tf_eflags |= FL_IOPL_3;
+	}
 }
 
 //
