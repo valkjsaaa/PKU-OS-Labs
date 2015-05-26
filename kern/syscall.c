7c8ad91f424721243f2f3082403a6050fbae7ef6
valkjsaaa <jackieyang51@gmail.com>
2015-05-26 12:22:52 +0800

    Lab 5: Exercise 7

--- kern/syscall.c
+++ kern/syscall.c
@@ -134,6 +134,17 @@ sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
 	// LAB 5: Your code here.
 	// Remember to check whether the user has supplied us with a good
 	// address!
+	struct Env *e;
+	int r;
+	if ((r = envid2env(envid, &e, 1)))
+	{
+		return r;
+	}
+	tf->tf_eip |= 0x3;
+	tf->tf_eflags |= FL_IF;
+
+	e->env_tf = *tf;
+	return 0;
 	panic("sys_env_set_trapframe not implemented");
 }
 
@@ -453,6 +464,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		return (int32_t)sys_ipc_try_send((envid_t)a1, (uint32_t)a2, (void *)a3, (unsigned)a4);
 	case SYS_ipc_recv:
 		return (int32_t)sys_ipc_recv((void *)a1);
+	case SYS_env_set_trapframe:
+		return (int32_t)sys_env_set_trapframe((envid_t) a1, (struct Trapframe *) a2);
 	default:
 		return -E_INVAL;
 	}
