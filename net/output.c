0087b12f10c32e5608a7c56375a7e7f3b736a4ea
valkjsaaa <jackieyang51@gmail.com>
2015-06-20 19:57:38 +0800

    Lab 6: Exercise 8

--- net/output.c
+++ net/output.c
@@ -10,4 +10,14 @@ output(envid_t ns_envid)
 	// LAB 6: Your code here:
 	// 	- read a packet from the network server
 	//	- send the packet to the device driver
+	uint32_t req;
+
+	while (1) {
+		req = ipc_recv(0, &nsipcbuf, 0);
+		if (req == NSREQ_OUTPUT) {
+			while(sys_net_xmit(nsipcbuf.pkt.jp_data, nsipcbuf.pkt.jp_len) < 0) {
+				sys_yield();
+			}
+		}
+	}
 }
