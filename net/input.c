36b1ed5806b3675442f5d839b67ea78f245f2a5d
valkjsaaa <jackieyang51@gmail.com>
2015-06-21 01:32:13 +0800

    Lab 6: Exercise 12

--- net/input.c
+++ net/input.c
@@ -13,4 +13,13 @@ input(envid_t ns_envid)
 	// Hint: When you IPC a page to the network server, it will be
 	// reading from it for a while, so don't immediately receive
 	// another packet in to the same physical page.
+
+	while(1) {
+	    while(sys_page_alloc(0, &nsipcbuf, PTE_U | PTE_P | PTE_W) < 0);
+	    while((nsipcbuf.pkt.jp_len = sys_net_recv(nsipcbuf.pkt.jp_data)) < 0) {
+	    		// cprintf("haha\n");
+				sys_yield();
+		}
+		while (sys_ipc_try_send(ns_envid, NSREQ_INPUT, &nsipcbuf, PTE_U | PTE_P | PTE_W) < 0);
+	}
 }
